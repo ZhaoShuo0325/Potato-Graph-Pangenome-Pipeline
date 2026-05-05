6113420b88cb718bc9c5b18fb488cdb246894f0c
ZhaoShuo <18904530325@163.com>
2026-05-05 19:57:27 +0800

    Update 00_3odgi_viz.sh

--- 01_work/00_3odgi_viz.sh
+++ 01_work/00_3odgi_viz.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-sed 's/50/128/g' work.sh | \
-sed 's/256G/256G/g' | \
-sed 's/edta/viz/g' | \
-sed 's/%j/viz/g' > viz.sh
-
-cat >> viz.sh << 'HEREDOC' 
 HOME="/public/home/zhaoshuo/work1"
 OG="$HOME/01_work/00_pggb/00_pggb_output/group60.chr01.fa.gz.c325321.7608fc1.877f7d9.smooth.final.og"
 OUT_DIR="$HOME/01_work/00_pggb/00_pggb_output/02_viz"
@@ -25,5 +18,3 @@ odgi viz -i $OG -t $t -o $OUT_DIR/${PREFIX}_depth_multiqc.png -x $x -y $y -a $a 
 odgi viz -i $OG -t $t -o $OUT_DIR/${PREFIX}_inv_multiqc.png -x $x -y $y -a $a -z -P -I "Consensus_" 2> >(tee -a $LOG_FILE)
 # stats
 odgi stats -t $t -S -i $OG > $OUT_DIR/${PREFIX}_odgi_stats.tsv 2> >(tee -a $LOG_FILE)
-HEREDOC
-sbatch viz.sh
