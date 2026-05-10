42550d5b7ebbfeffd2319e89b37d8d65dbe3c0ee
ZhaoShuo <18904530325@163.com>
2026-05-10 10:50:20 +0800

    Update and rename 00_3merge_vcf.sh to 03merge_vcf.sh

--- 01_work/01_MergeVCF/03merge_vcf.sh
+++ 01_work/01_MergeVCF/03merge_vcf.sh
@@ -4,14 +4,8 @@ REF="$HOME/data/reference/DM8.1_genome.ori.chr.fa"
 VCF="$HOME/01_work/data/1M_PAV/split_vcf"
 OUT_DIR="$HOME/01_work/data/1M_PAV/merged_vcf"
 CHR="$HOME/01_work/data/1M_PAV/split_vcf/chrs.txt"
+
 for chr in $(cat $CHR); do
-    sed "s/50/25/g" work.sh | \
-    sed "s/256G/50G/g" | \
-    sed "s/edta/$chr/g" | \
-    sed "s/%j/$chr/g" > $chr.sh
-
-
-cat >> $chr.sh << EOF
 ls ${VCF}/*.${chr}.vcf.gz > ${OUT_DIR}/${chr}_vcf.list
 MERGED_VCF=${OUT_DIR}/${chr}_merged.vcf.gz
 bcftools merge -l ${OUT_DIR}/${chr}_vcf.list | \
@@ -20,6 +14,4 @@ bcftools norm -d none -f $REF | \
 bcftools sort | \
 bgzip > \$MERGED_VCF
 tabix -p vcf \$MERGED_VCF
-EOF
-sbatch $chr.sh
 done
