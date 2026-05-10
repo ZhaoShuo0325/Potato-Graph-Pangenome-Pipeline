502f478155bc5bfbbc8b99eb2ae4e8283c48bba4
ZhaoShuo <18904530325@163.com>
2026-05-10 15:17:51 +0800

    Update and rename 01_work/01merge_graph.sh to 01_work/03_graph/01construct_graph.sh

--- 01_work/03_graph/01construct_graph.sh
+++ 01_work/03_graph/01construct_graph.sh
@@ -4,14 +4,7 @@ REF="$HOME/data/reference/DM8.1_genome.ori.chr.fa"
 VCF_DIR="$HOME/merge_test/00_data/04_mergeSV"
 CHR="$HOME/merge_test/00_data/chrs.txt"
 OUT_DIR="$HOME/merge_test/02_graph/01_merge"
+
 for chr in $(cat $CHR); do
-    sed "s/50/128/g" work.sh | \
-    sed "s/edta/merge_graph_$chr/g" | \
-    sed "s/%j/merge_graph_$chr/g" > merge_graph_$chr.sh
-
-
-cat >> merge_graph_$chr.sh << EOF
 varigraph construct -t 128 -r $REF -v $VCF_DIR/${chr}_merged.vcf.gz --save-graph $OUT_DIR/${chr}_graph.bin
-EOF
-sbatch merge_graph_$chr.sh
 done
