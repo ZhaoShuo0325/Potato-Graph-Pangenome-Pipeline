5aa4167a5affd6665837b79ea08e2f9420555ece
ZhaoShuo <18904530325@163.com>
2026-05-12 15:09:07 +0800

    Update 04run_genotyping.sh

--- 01_work/03_graph/04run_genotyping.sh
+++ 01_work/03_graph/04run_genotyping.sh
@@ -5,12 +5,8 @@ CHR="$HOME/merge_test/00_data/chrs.txt"
 FQ_DIR="$HOME/simulation/varsim_out"
 GRAPH_DIR="$HOME/merge_test/02_graph/01_merge"
 SAMPLE_FILE="$HOME/merge_test/02_graph/01_merge/genotyping/sample.cfg"
-for chr in $(cat $CHR); do
-    sed "s/50/128/g" work.sh | \
-    sed "s/edta/genotyping_$chr/g" | \
-    sed "s/%j/genotyping_$chr/g" > genotyping_$chr.sh
 
-cat >> genotyping_$chr.sh << EOF
+for chr in $(cat $CHR); do
 OUT_DIR="$HOME/merge_test/02_graph/01_merge/genotyping/${chr}"
 mkdir -p \$OUT_DIR
 cd \$OUT_DIR
@@ -20,16 +16,14 @@ varigraph genotype -t 128 \
                    --use-depth \
                    --sv \
                    -n 12
-EOF
-sbatch genotyping_$chr.sh
 done
 
 # varigraph 生成的 vcf.gz 不是 bgzip 压缩格式，需要手动修改
 
-for i in {01..12}; do
-    echo "正在修复 chr${i} 目录..."
-    for f in chr${i}/*.vcf.gz; do
-        zcat "$f" | bgzip -c > "${f}.tmp" && mv "${f}.tmp" "$f"
-        tabix -f -p vcf "$f"
-    done
-done
+#for i in {01..12}; do
+#    echo "正在修复 chr${i} 目录..."
+#    for f in chr${i}/*.vcf.gz; do
+#        zcat "$f" | bgzip -c > "${f}.tmp" && mv "${f}.tmp" "$f"
+#        tabix -f -p vcf "$f"
+#    done
+#done
