d81d982654e4c5476632cbc2830f6168418e1d90
ZhaoShuo <18904530325@163.com>
2026-05-13 15:01:22 +0800

    Add files via upload

---
+++ 01_work/01vg_construct.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+HOME="/public/home/zhaoshuo/work1"
+REF="$HOME/data/reference/DM8.1_genome.ori.chr.fa"
+VCF_DIR="$HOME/merge_test/00_data/04_mergeSV"
+CHR="$HOME/merge_test/00_data/chrs.txt"
+OUT_DIR="$HOME/merge_test/02_graph/04_vg_merge"
+for chr in $(cat $CHR); do
+    sed "s/50/128/g" work.sh | \
+    sed "s/edta/vg_$chr/g" | \
+    sed "s/%j/vg_$chr/g" > vg_$chr.sh
+
+cat >> vg_$chr.sh << EOF
+vg construct -t 128 \
+    -r $REF \
+    -R ${chr} \
+    -v $VCF_DIR/${chr}_merged.vcf.gz \
+    -a -p > $OUT_DIR/${chr}_graph.vg
+
+vg view -g $OUT_DIR/${chr}_graph.vg > $OUT_DIR/${chr}_graph.gfa
+vg gbwt --num-jobs 128 \
+    -x "$OUT_DIR/${chr}_graph.vg" \
+    -v "$VCF_DIR/${chr}_merged.vcf.gz" \
+    -o "$OUT_DIR/${chr}.gbwt" \
+    --vcf-variants \
+    --progress
+EOF
+sbatch vg_$chr.sh
+done
