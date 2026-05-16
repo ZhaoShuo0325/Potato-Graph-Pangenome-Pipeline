1310a549aaf89d2b819fbc9d00643ed98b72c915
ZhaoShuo <18904530325@163.com>
2026-05-16 15:21:31 +0800

    Update 04vg_pack.sh

--- 01_work/04_vg_scripts/04vg_pack.sh
+++ 01_work/04_vg_scripts/04vg_pack.sh
@@ -8,11 +8,6 @@ CHR="$HOME/merge_test/00_data/chrs.txt"
 OUT_DIR="$HOME/merge_test/02_graph/04_vg_merge/giraffe/giraffe_d${d}/vg_call"
 
 for chr in $(cat $CHR); do
-    sed "s/50/128/g" work.sh | \
-    sed "s/edta/pack_$chr/g" | \
-    sed "s/%j/pack_$chr/g" > pack_$chr.sh
-
-cat >> pack_$chr.sh << EOF
 LIST="$HOME/merge_test/00_data/real.txt"
 for SAMPLE in \$(cat \$LIST); do
     XG="$XG_DIR/vg_merge_${chr}.xg"
@@ -22,6 +17,4 @@ for SAMPLE in \$(cat \$LIST); do
         -g \$GAM \
         -o "$OUT_DIR/\${SAMPLE}_${chr}.pack"
 done
-EOF
-sbatch pack_$chr.sh
 done
