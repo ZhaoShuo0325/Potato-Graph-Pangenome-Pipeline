372ff6b193ea2c64cd479137de066962e4f44edd
ZhaoShuo <18904530325@163.com>
2026-06-30 08:35:59 +0800

    Update 04ROC_plot.sh

--- 01_work/05_compare_roc/04ROC_plot.sh
+++ 01_work/05_compare_roc/04ROC_plot.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-#SBATCH --partition=AMD_9A14
-#SBATCH --cpus-per-task=128
-#SBATCH --job-name=roc_plot
-#SBATCH --output=%x.out
-#SBATCH --error=%x.err
-
 #mamba install -c conda-forge r-tidyverse r-svglite --override-channels
 Rscript="/public/home/zhaoshuo/miniconda3/envs/R_env/bin/Rscript"
 GRAPH="/public/home/zhaoshuo/work1/merge_test/04_compare/01_sim_giraffe/roc_stats_giraffe.ft.tsv"
 BWA="/public/home/zhaoshuo/work1/merge_test/04_compare/03bwa_linear/roc_stats_bwa_bwa_linear.ft.tsv"
-GIRAFFE="/public/home/zhaoshuo/work1/merge_test/04_compare/02sim_linear/roc_stats_linear.ft.tsv"
+GIRAFFE="/public/home/zhaoshuo/work1/merge_test/04_compare/02sim_linear/roc_stats_graph-linear.ft.tsv"
 OUT_DIR="/public/home/zhaoshuo/work1/merge_test/04_compare/04roc_plot"
 mkdir -p $OUT_DIR
 
 head -n 1 $GRAPH > $OUT_DIR/all_roc_stats.ft.tsv
+tail -n +2 $GRAPH >> $OUT_DIR/all_roc_stats.ft.tsv
 tail -n +2 $GIRAFFE >> $OUT_DIR/all_roc_stats.ft.tsv
 tail -n +2 $BWA >> $OUT_DIR/all_roc_stats.ft.tsv
 
